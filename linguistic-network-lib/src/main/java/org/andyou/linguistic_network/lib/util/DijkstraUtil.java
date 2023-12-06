6d5ddb1aa67edb2e9ea04b64fe174447087e37fc
Frayks <andriy12345678910@gmail.com>
2023-12-06 22:51:37 +0200

    Development

--- linguistic-network-lib/src/main/java/org/andyou/linguistic_network/lib/util/DijkstraUtil.java
+++ linguistic-network-lib/src/main/java/org/andyou/linguistic_network/lib/util/DijkstraUtil.java
@@ -1,6 +1,7 @@
 package org.andyou.linguistic_network.lib.util;
 
 import org.andyou.linguistic_network.lib.api.data.ElementNode;
+import org.apache.commons.math3.util.Pair;
 
 import java.util.*;
 
@@ -8,47 +9,35 @@ public class DijkstraUtil {
 
     public static List<Double> calcPathLengths(ElementNode targetNode) {
         Map<ElementNode, Double> pathLengthMap = new HashMap<>();
-        Set<ElementNode> unvisitedNodes = new HashSet<>();
+        PriorityQueue<Pair<ElementNode, Double>> unvisitedNodes = new PriorityQueue<>(Comparator.comparingDouble(Pair::getValue));
 
         pathLengthMap.put(targetNode, 0.0);
-        unvisitedNodes.add(targetNode);
+        unvisitedNodes.add(new Pair<>(targetNode, 0.0));
 
         while (!unvisitedNodes.isEmpty()) {
-            ElementNode node = getMinPathLengthNode(unvisitedNodes, pathLengthMap);
-            double nodePathLength = pathLengthMap.get(node);
-            unvisitedNodes.remove(node);
+            Pair<ElementNode, Double> pairNode = unvisitedNodes.poll();
+            ElementNode node = pairNode.getKey();
+            double currentPathLength = pairNode.getValue();
+            double pathLength = pathLengthMap.get(node);
+
+            if (currentPathLength > pathLength) {
+                continue;
+            }
 
             for (Map.Entry<ElementNode, Integer> neighborEntry : node.getNeighbors().entrySet()) {
                 ElementNode neighborNode = neighborEntry.getKey();
                 double neighborDistance = 1.0 / neighborEntry.getValue();
-                double newNeighborPathLength = nodePathLength + neighborDistance;
+                double newNeighborPathLength = currentPathLength + neighborDistance;
 
                 Double neighborPathLength = pathLengthMap.get(neighborNode);
-                if (neighborPathLength == null) {
-                    unvisitedNodes.add(neighborNode);
+                if (neighborPathLength == null || newNeighborPathLength < neighborPathLength) {
                     pathLengthMap.put(neighborNode, newNeighborPathLength);
-                } else {
-                    if (newNeighborPathLength < neighborPathLength) {
-                        pathLengthMap.put(neighborNode, newNeighborPathLength);
-                    }
+                    unvisitedNodes.offer(new Pair<>(neighborNode, newNeighborPathLength));
                 }
             }
         }
         pathLengthMap.remove(targetNode);
         return new ArrayList<>(pathLengthMap.values());
-    }
-
-    private static ElementNode getMinPathLengthNode(Set<ElementNode> elementNodes, Map<ElementNode, Double> pathLengthMap) {
-        ElementNode minPathLengthElementNode = null;
-        double minPathLength = Integer.MAX_VALUE;
-        for (ElementNode elementNode : elementNodes) {
-            double pathLength = pathLengthMap.get(elementNode);
-            if (pathLength < minPathLength) {
-                minPathLengthElementNode = elementNode;
-                minPathLength = pathLength;
-            }
-        }
-        return minPathLengthElementNode;
     }
 
 }
